cf56d0ccdc10be7073a341f8666f9c55e124ff5d
Ian Webster <ianw_git@ianww.com>
2015-05-13 23:39:41 -0700

    server and working stripe

--- neutralgas_app.js
+++ neutralgas_app.js
@@ -1,26 +1,46 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 var stripe = require('stripe')(
   require('./config.js').STRIPE_SK
 );
 
+app.get('/', function(req, res) {
+  serveFile('index.html', res);
+});
+
+app.get('/order', function(req, res) {
+  serveFile('order.html', res);
+});
+
+app.get('/transparency', function(req, res) {
+  serveFile('transparency.html', res);
+});
+
 app.get('/pay', function (req, res) {
   stripe.charges.create({
     amount: req.query.amount,
     currency: 'usd',
     source: req.query.tok,
-    email: req.query.email,
     description: 'Charge for micro carbon offset - ' + req.query.email,
   }, function(err, charge) {
-    // asynchronously called
     if (err) {
+      console.log(err, charge);
       res.send('error');
       return;
     }
     res.send('ok');
   });
 });
+
+function serveFile(path, res) {
+  fs.readFile(path, function (err, data){
+    res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+    res.write(data);
+    res.end();
+  });
+}
 
 var server = app.listen(4111, function () {
   var host = server.address().address;
